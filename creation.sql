8528c3eb0cd6c92f2d7a8a554caa0f972d00d5e4
Yegor Bitensky <yegorbitensky@gmail.com>
2021-08-04 02:04:49 +0300

    Initial

---
+++ creation.sql
@@ -0,0 +1,6 @@
+CREATE TABLE test (
+    done boolean DEFAULT false
+);
+
+
+INSERT INTO test DEFAULT VALUES;
---
+++ python/code/src/main.py
@@ -0,0 +1,34 @@
+import asyncio
+import aiopg
+import logging
+import os
+
+
+CONNECTION_DATA = {
+    'dbname': os.environ.get('POSTGRES_DB'),
+    'user': os.environ.get('POSTGRES_USER'),
+    'password': os.environ.get('POSTGRES_PASSWORD'),
+    'host': 'postgres',
+}
+dsn = ' '.join(map(lambda i: f'{i[0]}={i[1]}', CONNECTION_DATA.items()))
+
+
+async def get_data(cur):
+    await cur.execute('SELECT * FROM test;')
+    ret = []
+    async for row in cur:
+        ret.append(row)
+    logging.warning(ret)
+
+
+async def go():
+    async with aiopg.create_pool(dsn) as pool:
+        async with pool.acquire() as conn:
+            async with conn.cursor() as cur:
+                while True:
+                    await get_data(cur)
+                    await asyncio.sleep(1)
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(go())
