fdd2d70c6b1c92ec3535c1d4fd9c72fa95aa1e3c
Yegor Bitensky <yegorbitensky@gmail.com>
2021-08-07 21:39:24 +0300

    Add messages handle logic

--- python/code/src/main.py
+++ python/code/src/main.py
@@ -13,24 +13,54 @@ CONNECTION_DATA = {
 dsn = ' '.join(map(lambda i: f'{i[0]}={i[1]}', CONNECTION_DATA.items()))
 
 
+async def handle_message(cur, id):
+    logging.warning(f'Get mesage with id = {id}')
+    await cur.execute(
+        f'''
+        UPDATE test_message
+        SET sending = true
+        WHERE id = {id};
+        '''
+    )
+    if cur.rowcount > 0:
+        logging.warning(f'Sending mesage with id = {id}')
+        await asyncio.sleep(30)
+        await cur.execute(
+            f'''
+            UPDATE test_message
+            SET (sending, done) = (false, true)
+            WHERE id = {id};
+            '''
+        )
+
+
 async def get_data(cur):
-    await cur.execute('SELECT * FROM test;')
-    ret = []
-    async for row in cur:
-        ret.append(row)
-    logging.warning(ret)
-    await cur.execute('UPDATE test SET done = true WHERE done = false;')
-    logging.warning(cur.rowcount)
+    await cur.execute(
+        '''
+        SELECT id, done, sending
+        FROM test_message
+        WHERE done = false AND sending = false;
+        '''
+    )
+    logging.warning(f'cur.rowcount {cur.rowcount}')
+    if cur.rowcount == 0:
+        return
+    data = await cur.fetchall()
+    for id, done, sending in data:
+        await handle_message(cur, id)
 
 
-async def go():
+async def go(timeout):
+    await asyncio.sleep(timeout)
     async with aiopg.create_pool(dsn) as pool:
         async with pool.acquire() as conn:
             async with conn.cursor() as cur:
-                while True:
-                    await get_data(cur)
-                    await asyncio.sleep(1)
+                await get_data(cur)
 
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(go())
+async def main():
+    await asyncio.gather(*(go(i) for i in range(100)))
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
