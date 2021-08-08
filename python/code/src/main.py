e5498038405480a7f833f267fab33af5c6820d8f
Yegor Bitensky <yegorbitensky@gmail.com>
2021-08-09 01:28:28 +0300

    Add message get handler

--- python/code/src/main.py
+++ python/code/src/main.py
@@ -6,45 +6,61 @@ import os
 
 class MessageHandler:
 
-    def __init__(self, app, id):
+    def __init__(self, app):
         self._app = app
-        self._id = id
         self._cur = None
 
     async def start(self):
-        await asyncio.sleep(self._id / self._app.threads_count)
         async with aiopg.create_pool(self._app.dsn) as pool:
             async with pool.acquire() as conn:
                 async with conn.cursor() as cur:
                     self._cur = cur
                     while True:
-                        await self.get_data()
+                        await self._handle()
                         await asyncio.sleep(1)
 
-    async def get_data(self):
+
+class MessageGetHandler(MessageHandler):
+    async def _handle(self):
         await self._cur.execute(
             '''
             SELECT id, done, sending
             FROM test_message
-            WHERE done = false AND sending = false
-            ORDER BY id ASC
-            LIMIT 1;
+            WHERE done = false AND sending = false;
             '''
         )
-        logging.warning(f'{self._id} rowcount {self._cur.rowcount}')
+        logging.warning(f'GETTER rowcount {self._cur.rowcount}')
         if self._cur.rowcount == 0:
             return
-        id, done, sending = await self._cur.fetchone()
-        await self.handle_message(id)
+        for id, done, sending in await self._cur.fetchall():
+            self._app.data[id] = {
+                'done': done,
+                'sending': sending,
+            }
 
-    async def handle_message(self, id):
+
+class MessageSendHandler(MessageHandler):
+
+    def __init__(self, app, id):
+        super().__init__(app)
+        self._id = id
+
+    async def start(self):
+        await asyncio.sleep(self._id / self._app.threads_count)
+        await super().start()
+
+    async def _handle(self):
+        if not self._app.data:
+            return
+        id, values = self._app.data.popitem()
         await self._reserve_message(id)
-        if self._cur.rowcount > 0:
-            await self._send_message(id)
-            await self._complete_message(id)
+        if self._cur.rowcount == 0:
+            return
+        await self._send_message(id)
+        await self._complete_message(id)
 
     async def _reserve_message(self, id):
-        logging.warning(f'{self._id} Reserve mesage with id = {id}')
+        logging.warning(f'SENDER {self._id} Reserve mesage with id = {id}')
         await self._cur.execute(
             f'''
             UPDATE test_message
@@ -54,11 +70,11 @@ class MessageHandler:
         )
 
     async def _send_message(self, id):
-        logging.warning(f'{self._id} Sending mesage with id = {id}')
+        logging.warning(f'SENDER {self._id} Sending mesage with id = {id}')
         await asyncio.sleep(30)
 
     async def _complete_message(self, id):
-        logging.warning(f'{self._id} Complete mesage with id = {id}')
+        logging.warning(f'SENDER {self._id} Complete mesage with id = {id}')
         await self._cur.execute(
             f'''
             DELETE
@@ -77,7 +93,11 @@ class App:
 
     async def main(self):
         await asyncio.gather(
-            *(MessageHandler(self, i).start() for i in range(self.threads_count))
+            MessageGetHandler(self).start(),
+            *(
+                MessageSendHandler(self, i).start()
+                for i in range(self.threads_count
+            ))
         )
 
 
