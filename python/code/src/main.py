9acfea38bf6c26435c9021dbee07e869943f6b28
Yegor Bitensky <yegorbitensky@gmail.com>
2021-08-10 01:28:01 +0300

    Improve MessagesGetter and MessagesHandler

--- python/code/src/main.py
+++ python/code/src/main.py
@@ -28,35 +28,24 @@ def with_cursor(method):
 
 class MessageHandler:
 
-    def __init__(self, id, message_id):
+    def __init__(self, app, id, message_id):
+        self._app = app
         self._id = id
         self._message_id = message_id
         self._cur = None
 
     @with_cursor
     async def start(self):
-        await self._reserve_message()
-        if self._cur.rowcount == 0:
-            return
         await self._send_message()
         await self._complete_message()
-
-    async def _reserve_message(self):
-        logging.warning(f'SENDER {self._id} Reserve mesage with id = {self._message_id}')
-        await self._cur.execute(
-            f'''
-            UPDATE test_message
-            SET sending = true
-            WHERE id = {self._message_id};
-            '''
-        )
 
     async def _send_message(self):
         logging.warning(f'SENDER {self._id} Sending mesage with id = {self._message_id}')
         await asyncio.sleep(30)
+        logging.warning(f'SENDER {self._id} Send mesage with id = {self._message_id}')
 
     async def _complete_message(self):
-        logging.warning(f'SENDER {self._id} Complete mesage with id = {self._message_id}')
+        logging.warning(f'SENDER {self._id} Completing mesage with id = {self._message_id}')
         await self._cur.execute(
             f'''
             DELETE
@@ -64,6 +53,8 @@ class MessageHandler:
             WHERE id = {self._message_id};
             '''
         )
+        self._app.done_message(self._message_id)
+        logging.warning(f'SENDER {self._id} Complete mesage with id = {self._message_id}')
 
 
 class MessagesHandler:
@@ -81,9 +72,10 @@ class MessagesHandler:
     async def _handle(self):
         if not self._app.data:
             return
+        logging.warning(f'HANDLER {self._id} Handle {self._app.data}')
         handlers = (
-            MessageHandler(self._id, id).start()
-            for id, done, sending in self._app.data
+            MessageHandler(self._app, self._id, d[0]).start()
+            for d in self._app.data
         )
         self._app.data = None
         await asyncio.gather(*handlers)
@@ -102,17 +94,22 @@ class MessagesGetter:
             await asyncio.sleep(1)
 
     async def _handle(self):
+        logging.warning(f'data {self._app.data}, fetching {self._app.fetching}, processing_ids {self._app.processing_ids}')
+        if self._app.fetching or self._app.data:
+            return
         await self._cur.execute(
-            '''
-            SELECT id, done, sending
+            f"""
+            SELECT id
             FROM test_message
-            WHERE done = false AND sending = false;
-            '''
+            WHERE NOT (id = ANY ('{self._app.processing_ids or '{}'}'));
+            """
         )
         logging.warning(f'GETTER rowcount {self._cur.rowcount}')
         if self._cur.rowcount == 0:
             return
-        self._app.data = await self._cur.fetchall()
+        self._app.fetching = True
+        self._app.set_data(await self._cur.fetchall())
+        self._app.fetching = False
 
 
 class App:
@@ -120,12 +117,21 @@ class App:
     def __init__(self, handlers_count=5):
         self._handlers_count = handlers_count
         self.data = None
+        self.fetching = False
+        self.processing_ids = set()
 
     async def main(self):
         await asyncio.gather(
             MessagesGetter(self).start(),
             *(MessagesHandler(self, i).start() for i in range(self._handlers_count)),
         )
+
+    def set_data(self, data):
+        self.data = data
+        self.processing_ids |= {d[0] for d in data}
+
+    def done_message(self, id):
+        self.processing_ids -= {id}
 
 
 if __name__ == '__main__':
